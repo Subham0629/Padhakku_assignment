1411a243b398037d029253ccd85cb698d3c1aa51
Subham0629 <112814727+Subham0629@users.noreply.github.com>
2023-10-06 12:14:00 +0530

    cors added

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 const postRoutes = require("./routes/postRoutes");
+const cors = require('cors');
 require('dotenv').config()
+
 const app = express();
 app.use(cors());
 // Connect to MongoDB
